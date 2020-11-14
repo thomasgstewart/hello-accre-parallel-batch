3ad48c90e8647403d524269219f43c55c05a02e5
Thomas Gordon Stewart <stewatg1@cn425.vampire>
2020-11-13 22:41:11 -0600

    3

--- 04-parallel-accre-array.R
+++ 04-parallel-accre-array.R
@@ -4,18 +4,20 @@ args <- as.numeric(args)
 
 # Load packages
 .libPaths("~/R/rlib")
+require(magrittr)
 source("01-functions.R")
-sim_settings <- readRDS("sim-settings.RDS")
 
 # Select row of sim
-params <- readRDS("sim-settings.RdS")[args,] %>% as.list
+sim_settings <- readRDS("sim-settings.RDS")
+params <- sim_settings[args,] %>% as.list
 
 # Set seed
 set.seed(args)
 
 # Simulation
-sim_settings[args, c("miss_left", "capture", "miss_right")] <- repM(params)
+sim_settings[args, c("miss_left", "capture", "miss_right")] <- Mreps(params)
 
 # Save to file
 outfile <- "./results/sim-results-" %|% Sys.Date() %|% "-" %|% sprintf("%03.0f", args) %|% ".rds"
 saveRDS(sim_settings[args, ], file = outfile)
+

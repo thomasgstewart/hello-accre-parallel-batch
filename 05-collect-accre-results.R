1a2bd532bb53eab0c4e3b77e968d4e9ac7b6b4d3
Thomas Gordon Stewart <stewatg1@gw342.vampire>
2020-11-14 13:53:13 -0600

    8

--- 05-collect-accre-results.R
+++ 05-collect-accre-results.R
@@ -1,3 +1,5 @@
+require(magrittr)
+
 # Creates a single data.table with all the simulation results from the same date
 setwd("./results")
 f1 <- list.files(pattern = "sim-results-")
@@ -5,7 +7,7 @@ f2 <- lapply(f1, readRDS)
 f3 <- data.table::rbindlist(f2)
 setwd("..")
 
-date_time <- Sys.time() %>% format(format = "%Y-%m-%d-%H-%M-%S",.)
+date_time <- Sys.time() %>% format(format = "%Y-%m-%d-%H-%M-%S")
 "sim-results-DATE.RDS" %>% 
-  gsub("DATE",date_time) %>%
+  gsub("DATE",date_time,.) %>%
   saveRDS(f3, .)
